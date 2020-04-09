be709bab47fcbfe10ad6ac8ba5af6605e709637c
wongchishan <wongchishan99@gmail.com>
2020-04-09 22:13:53 +0800

    Update delete workout exercise command test

--- src/test/java/seedu/zerotoone/logic/commands/workout/exercise/DeleteCommandTest.java
+++ src/test/java/seedu/zerotoone/logic/commands/workout/exercise/DeleteCommandTest.java
@@ -2,6 +2,7 @@ package seedu.zerotoone.logic.commands.workout.exercise;
 
 import static seedu.zerotoone.testutil.CommandTestUtil.assertCommandSuccess;
 import static seedu.zerotoone.testutil.TypicalIndexes.INDEX_FIRST_OBJECT;
+import static seedu.zerotoone.testutil.TypicalIndexes.INDEX_SECOND_OBJECT;
 import static seedu.zerotoone.testutil.exercise.TypicalExercises.getTypicalExerciseList;
 import static seedu.zerotoone.testutil.workout.TypicalWorkouts.getTypicalWorkoutList;
 
@@ -15,6 +16,9 @@ import seedu.zerotoone.model.schedule.ScheduleList;
 import seedu.zerotoone.model.userprefs.UserPrefs;
 import seedu.zerotoone.model.workout.Workout;
 import seedu.zerotoone.testutil.workout.WorkoutBuilder;
+import static seedu.zerotoone.testutil.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DeleteCommandTest {
     private Model model = new ModelManager(new UserPrefs(),
@@ -22,6 +26,11 @@ public class DeleteCommandTest {
             getTypicalWorkoutList(),
             new ScheduleList(),
             new LogList());
+
+    @Test
+    public void constructor_nullWorkout_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new DeleteCommand(null, null));
+    }
 
     @Test
     public void execute_validIndexUnfilteredList_success() {
@@ -46,5 +55,27 @@ public class DeleteCommandTest {
         expectedModel.setWorkout(workoutToEdit, editedWorkout);
 
         assertCommandSuccess(deleteCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void equals() {
+        DeleteCommand DeleteCommand = new DeleteCommand(INDEX_FIRST_OBJECT, INDEX_FIRST_OBJECT);
+        DeleteCommand otherDeleteCommand = new DeleteCommand(INDEX_FIRST_OBJECT, INDEX_SECOND_OBJECT);
+
+        // same object -> returns true
+        assertTrue(DeleteCommand.equals(DeleteCommand));
+
+        // same values -> returns true
+        DeleteCommand DeleteCommandCopy = new DeleteCommand(INDEX_FIRST_OBJECT, INDEX_FIRST_OBJECT);
+        assertTrue(DeleteCommand.equals(DeleteCommandCopy));
+
+        // different types -> returns false
+        assertFalse(DeleteCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(DeleteCommand.equals(null));
+
+        // different workout -> returns false
+        assertFalse(DeleteCommand.equals(otherDeleteCommand));
     }
 }
